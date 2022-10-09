2252e90bf95925f44bbbceb6e28bea5586e37f88
hannal <kay@hannal.net>
2022-10-09 16:06:12 +0900

    ADD 구현 스펙에 맞춰 의사 코드로 구현. 테스트는 실패.

--- python/pseudo-code-based-learning-fastapi/tests/apps/reservation/test_controllers.py
+++ python/pseudo-code-based-learning-fastapi/tests/apps/reservation/test_controllers.py
@@ -1,14 +1,17 @@
+from apps.reservation.controllers import 예약가능한세션목록가져오기
 
 
 def test_예약가능한세션목록가져오기():
     # - 주어진 조건 (Given)
     #   - 로그인 한 고객
     #   - 예약 항목 2개
+    user = "로그인 한 고객"
+    items = ["예약 항목 1", "예약 항목 2"]
 
     # - 수행 (When)
     #   - 예약 가능한 세션 목록을 가져오기
+    result = 예약가능한세션목록가져오기(user)
 
     # - 기대하는 결과 (Then)
     #   - 예약 항목 2개를 목록으로 반환
-    pass
-
+    assert items == result
