918818215f193eb7292ee0ef1f9f7cf85249d71b
hannal <kay@hannal.net>
2022-10-10 02:06:55 +0900

    ADD db boilerplate for testing.

--- python/pseudo-code-based-learning-fastapi/tests/apps/reservation/conftest.py
+++ python/pseudo-code-based-learning-fastapi/tests/apps/reservation/conftest.py
@@ -2,12 +2,19 @@ import pytest
 from fastapi import FastAPI
 from fastapi.testclient import TestClient
 
+from db import use_db_session, get_session_factory
 from apps.reservation.controllers import router
 
 
 @pytest.fixture
-def fastapi_app():
+def fastapi_app(db_engine):
+    async def testing_use_db_session():
+        session_factory = await get_session_factory(db_engine)
+        async with session_factory() as _session:
+            yield _session
+
     app = FastAPI()
+    app.dependency_overrides[use_db_session] = testing_use_db_session
     app.include_router(router)
     yield app
 
--- python/pseudo-code-based-learning-fastapi/tests/conftest.py
+++ python/pseudo-code-based-learning-fastapi/tests/conftest.py
@@ -2,6 +2,9 @@ import sys, asyncio
 
 import pytest
 
+from main import load_models
+from db import create_engine, get_session_factory, BaseModel
+
 
 @pytest.fixture(scope="session")
 def event_loop():
@@ -13,3 +16,38 @@ def event_loop():
     loop = asyncio.get_event_loop_policy().new_event_loop()
     yield loop
     loop.close()
+
+
+DEFAULT_TEST_DB_DSN = "sqlite+aiosqlite:///:memory:"
+
+DEFAULT_TEST_DB_SESSION_OPTIONS = {
+    "autocommit": False,
+    "expire_on_commit": False,
+    "autoflush": False,
+}
+
+DEFAULT_TEST_DB_ECHO = False
+
+
+@pytest.fixture(scope="session")
+async def db_engine():
+    load_models()
+    _engine = await create_engine(DEFAULT_TEST_DB_DSN, {}, echo=DEFAULT_TEST_DB_ECHO)
+
+    async with _engine.begin() as conn:
+        await conn.run_sync(BaseModel.metadata.drop_all)
+
+    async with _engine.begin() as conn:
+        await conn.run_sync(BaseModel.metadata.create_all)
+        yield conn
+        await conn.rollback()
+
+    _engine.sync_engine.dispose()
+
+
+@pytest.fixture(scope="session")
+async def db_session(db_engine):
+    session_factory = await get_session_factory(db_engine)
+    async with session_factory() as _session:
+        yield _session
+        await _session.rollback()
