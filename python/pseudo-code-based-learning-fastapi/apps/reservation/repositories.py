adfb25a68eece8ee8268cf7d8a8c08db32e99166
hannal <kay@hannal.net>
2022-10-09 16:18:06 +0900

    ADD 예약 항목 저장소로 예약 항목 생성 기능 구현.

---
+++ python/pseudo-code-based-learning-fastapi/apps/reservation/repositories.py
@@ -0,0 +1,21 @@
+# ReservationRepository객체
+#     def create(예약_항목_생성에_필요한_데이터):
+#         새_예약_항목 = 영속_데이터_생성(예약_항목_생성에_필요한_데이터) 영속_데이터_저장(새_예약_항목)
+#         새_예약_항목.id = 고유한_일련번호_값
+#     return 새_예약_항목
+
+
+items = []
+
+
+class ReservationRepository:
+    _items: list
+
+    def __init__(self):
+        self._items = items
+
+    def create(self, payload):
+        obj = payload
+        obj.id = len(self._items) + 1
+        self._items.append(obj)
+        return obj
