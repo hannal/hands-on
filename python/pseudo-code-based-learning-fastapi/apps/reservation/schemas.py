28547436fc34f56eabee0d7d79946aea58e4e041
hannal <kay@hannal.net>
2022-10-10 02:33:51 +0900

    ADD ReservationListParam schema. validate_scheduled_date() 함수는 schemas.py 로 위치 조정.

--- python/pseudo-code-based-learning-fastapi/apps/reservation/schemas.py
+++ python/pseudo-code-based-learning-fastapi/apps/reservation/schemas.py
@@ -17,3 +17,20 @@ class Reservation(pydantic.BaseModel):
 
     def __hash__(self):
         return hash(id)
+
+
+class ReservationListParam(pydantic.BaseModel):
+    scheduled_date: datetime.date
+
+    @pydantic.validator("scheduled_date", pre=True, always=True)
+    def validate_scheduled_date(cls, v: datetime.date):
+        return validate_scheduled_date(v)
+
+
+def validate_scheduled_date(v: datetime.date):
+    today = datetime.date.today()
+    if v.year < today.year or v.month < today.month:
+        raise ValueError("오늘이거나 이후 일자여야 합니다.")
+    if v.year == today.year and v.month == today.month:
+        return today
+    return v
--- python/pseudo-code-based-learning-fastapi/apps/reservation/services.py
+++ python/pseudo-code-based-learning-fastapi/apps/reservation/services.py
@@ -1,19 +1,12 @@
 import datetime
 
+from libs.fastapi.dependencies import User
 from apps.reservation.repositories import BaseReservationRepository
+from apps.reservation.schemas import validate_scheduled_date
 
 
 async def reservations(
-    user, repository: BaseReservationRepository, scheduled_date: datetime.date
+    user: User, repository: BaseReservationRepository, scheduled_date: datetime.date
 ):
     scheduled_date = validate_scheduled_date(scheduled_date)
     return await repository.findall(scheduled_date=scheduled_date)
-
-
-def validate_scheduled_date(v: datetime.date):
-    today = datetime.date.today()
-    if v.year < today.year or v.month < today.month:
-        raise ValueError("오늘이거나 이후 일자여야 합니다.")
-    if v.year == today.year and v.month == today.month:
-        return today
-    return v
