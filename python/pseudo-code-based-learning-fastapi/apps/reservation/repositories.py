70800b34d0ec47e8fd4cdc0a23a49aaadcca7159
hannal <kay@hannal.net>
2022-10-09 16:31:22 +0900

    ADD ReservationRepository.findall 구현, 테스트 코드.

--- python/pseudo-code-based-learning-fastapi/apps/reservation/repositories.py
+++ python/pseudo-code-based-learning-fastapi/apps/reservation/repositories.py
@@ -3,6 +3,9 @@
 #         새_예약_항목 = 영속_데이터_생성(예약_항목_생성에_필요한_데이터) 영속_데이터_저장(새_예약_항목)
 #         새_예약_항목.id = 고유한_일련번호_값
 #     return 새_예약_항목
+#
+#     def findall():
+#         return list(저장된_영속 데이터들)
 
 
 items = []
@@ -19,3 +22,6 @@ class ReservationRepository:
         obj.id = len(self._items) + 1
         self._items.append(obj)
         return obj
+
+    def findall(self):
+        return self._items
--- python/pseudo-code-based-learning-fastapi/tests/apps/reservation/test_repositories.py
+++ python/pseudo-code-based-learning-fastapi/tests/apps/reservation/test_repositories.py
@@ -22,3 +22,20 @@ def test_repository_can_create_reservation_with_valid_payload():
     #   - 고유한 일련번호 값이 `id` 속성으로 부여된 새로운 객체 반환
     assert hasattr(result, "id")
     assert isinstance(result.id, int)
+
+
+def test_repository_find_all_items_without_params():
+    # 주어진 조건
+    #   - 예약 항목 2개
+    repository = ReservationRepository()
+    item = repository.create(ReservationPayload())
+
+    # 수행
+    #   - 예약 항목 목록을 가져오기
+    result = repository.findall()
+
+    # 기대하는 결과
+    #   - 저장된 예약 항목 전체를 목록으로 반환
+    assert isinstance(item.id, int)
+    assert len(result) > 0
+    assert any([_o.id == item.id for _o in result])
