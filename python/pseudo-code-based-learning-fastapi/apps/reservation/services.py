f416d65d996720886e99575f6fe6cd2c1b5a58be
hannal <kay@hannal.net>
2022-10-09 17:39:33 +0900

    ADD reservation service 에 scheduled_date 인자 추가 기존 테스트에도 반영.

--- python/pseudo-code-based-learning-fastapi/apps/reservation/services.py
+++ python/pseudo-code-based-learning-fastapi/apps/reservation/services.py
@@ -1,5 +1,19 @@
+import datetime
+
 from apps.reservation.repositories import ReservationRepository
 
 
-def reservations(user, repository: ReservationRepository):
+def reservations(
+    user, repository: ReservationRepository, scheduled_date: datetime.date
+):
+    validate_scheduled_date(scheduled_date)
     return repository.findall()
+
+
+def validate_scheduled_date(v: datetime.date):
+    today = datetime.date.today()
+    if v.year < today.year or v.month < today.month:
+        raise ValueError("오늘이거나 이후 일자여야 합니다.")
+    if v.year == today.year and v.month == today.month:
+        return today
+    return v
