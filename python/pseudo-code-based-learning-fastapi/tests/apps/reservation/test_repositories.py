9e095c8b09afc56b3e9e077979df6f02d307017b
hannal <kay@hannal.net>
2022-10-09 16:26:16 +0900

    ADD ReservationRepository.create 구현에 대한 테스트 코드.

--- python/pseudo-code-based-learning-fastapi/tests/apps/reservation/test_repositories.py
+++ python/pseudo-code-based-learning-fastapi/tests/apps/reservation/test_repositories.py
@@ -1,10 +1,24 @@
-def test_예약_항목_영속_데이터_저장소로_예약_항목_생성_성공():
+import dataclasses
+
+from apps.reservation.repositories import ReservationRepository
+
+
+@dataclasses.dataclass
+class ReservationPayload:
+    id: int | None = dataclasses.field(default=None)
+
+
+def test_repository_can_create_reservation_with_valid_payload():
     # 주어진 조건
     #   - 유효한 예약 항목 생성 데이터
+    payload = ReservationPayload()
 
     # 수행
     #   - 예약 항목 생성
+    repository = ReservationRepository()
+    result = repository.create(payload)
 
     # 기대하는 결과
     #   - 고유한 일련번호 값이 `id` 속성으로 부여된 새로운 객체 반환
-    pass
+    assert hasattr(result, "id")
+    assert isinstance(result.id, int)
