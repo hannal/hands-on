f238b4f7a25f6daee8587627dca985c40445d044
Kay Cha <kay@hannal.net>
2024-03-14 11:05:52 +0900

    Add: product app

---
+++ python/django5-hipstacks/puddingcamp/apps/product/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Price(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.product_id} - {self.price}'
