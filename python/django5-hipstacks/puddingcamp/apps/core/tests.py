e8005e4e64f38d5a115577d214456c565aaf1744
Kay Cha <kay@hannal.net>
2024-03-14 15:41:36 +0900

    Add: 상품에 가격 표시.

--- python/django5-hipstacks/puddingcamp/apps/core/tests.py
+++ python/django5-hipstacks/puddingcamp/apps/core/tests.py
@@ -1,3 +1,22 @@
+from asgiref.sync import async_to_sync
 from django.test import TestCase
+from django.test.client import AsyncClient
+from django.urls import reverse
 
-# Create your tests here.
+from apps.product.services import ProductService
+
+
+class TestProductView(TestCase):
+    client_class = AsyncClient
+    service = ProductService()
+
+    def setUp(self) -> None:
+        async_to_sync(self.service.create_product_with_price)("product 1", 299_792.458)
+        async_to_sync(self.service.create_product_with_price)("product 2", 980_665)
+
+    async def test_product_list(self) -> None:
+        url = reverse("product:product-list")
+        res = await self.client.get(url)
+        assert res.status_code == 200
+
+        self.assertContains(res, '<div id="product-price-"')
--- python/django5-hipstacks/puddingcamp/apps/product/views.py
+++ python/django5-hipstacks/puddingcamp/apps/product/views.py
@@ -1,6 +1,14 @@
 from django.http import HttpRequest
 from django.shortcuts import render
 
+from .services import ProductService
+
 
 async def product_list(request: HttpRequest) -> None:
-    return render(request, "product_list.html", {})
+    service = ProductService()
+    products = await service.get_products()
+
+    ctx = {
+        "products": products,
+    }
+    return render(request, "product_list.html", ctx)
